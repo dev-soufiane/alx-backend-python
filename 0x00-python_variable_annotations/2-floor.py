6cb698f9d725e01447dce16b619671a88bb9804b
dev-soufiane <dev.sofyane@gmail.com>
2024-06-06 23:30:05 +0100

    Add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a function to calculate the floor value of a float."""
+
+
+import math
+
+
+def calculate_floor(n: float) -> int:
+    """Returns the floor value of the input float."""
+    return math.floor(n)
