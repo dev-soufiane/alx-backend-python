65150722da98752676b6fea5d6156efbd689fd46
dev-soufiane <dev.sofyane@gmail.com>
2024-06-07 00:33:44 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """This script defines a function to concatenate two strings."""
 
 
-def concatenate_strings(string1: str, string2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """Combines two strings together and returns the result."""
-    return string1 + string2
+    return str1 + str2
