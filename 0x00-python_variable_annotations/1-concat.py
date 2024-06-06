580b1673bab2af5f5598914f45ba279ed4e3f691
dev-soufiane <dev.sofyane@gmail.com>
2024-06-06 23:27:54 +0100

    Add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script defines a function to concatenate two strings."""
+
+
+def concatenate_strings(string1: str, string2: str) -> str:
+    """Combines two strings together and returns the result."""
+    return string1 + string2
