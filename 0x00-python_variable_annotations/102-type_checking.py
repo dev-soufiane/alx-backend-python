c939b084c39c2a87a5d9255b58f50577907d03fd
dev-soufiane <dev.sofyane@gmail.com>
2024-06-07 00:29:18 +0100

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python3
-"""
-Zooms in on a tuple by repeating its elements.
+"""Use mypy to validate the piece of code and apply any changes."""
 
-Parameters:
-    lst (Tuple): The tuple to zoom in on.
-    factor (int): The factor by which to zoom in. Default is 2.
-
-Returns:
-    Tuple: The zoomed-in tuple.
-"""
-
-from typing import Tuple
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    """Zooms in on a tuple by repeating its elements."""
-    zoomed_in: Tuple = tuple(
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of integers multiplied by a certain factor."""
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    )
+    ]
     return zoomed_in
 
-
-array = (12, 72, 91)
+array: Tuple[int, ...] = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-
-print(zoom_array.__annotations__)
